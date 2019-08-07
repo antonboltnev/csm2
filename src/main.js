2af703474804e6bbb5fdf783dfb3bef3582b3a7c
Болтнев Антон Андреевич <ABoltnev@sportmaster.ru>
2019-08-07 08:17:48 +0300

    vuetify

--- src/main.js
+++ src/main.js
@@ -2,19 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store/store.js'
 import router from './router/router.js'
-import Vuetify from 'vuetify'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
-import 'vuetify/dist/vuetify.min.css'
+import vuetify from './plugins/vuetify';
 
 
-Vue.use(Vuetify, {
-    iconfont: 'md'
-})
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
-    router,
-    store
+  router,
+  vuetify,
+  store
 }).$mount('#app')
